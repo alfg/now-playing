46d407ab2a5ccb826a5df527e354df099931eaab
Alf <alf.g.jr@gmail.com>
2019-04-16 00:50:56 -0700

    fixed?

--- app.js
+++ app.js
@@ -4,17 +4,17 @@ const express = require('express')
 const http = require('http');
 const morgan = require('morgan');
 const api = require('./controllers/api');
-const WebSocket = require('ws');
 const history = require('connect-history-api-fallback');
 
 
 const app = express()
 const port = process.env.PORT || 4000
 
-app.use(history());
 app.use(morgan('combined'));
-app.use(express.static('static/dist'))
+
 app.use('/api', api);
+app.use(history());
+app.use(express.static('static/dist'))
 
 app.listen(port, () => console.log(`app listening on port ${port}!`));
 
