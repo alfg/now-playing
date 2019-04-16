f190aad6b1831999ded08e5f5a0e3c81d732bb62
Alf <alf.g.jr@gmail.com>
2019-04-16 00:15:46 -0700

    add connect-history api fallback fix for routing.

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ const http = require('http');
 const morgan = require('morgan');
 const api = require('./controllers/api');
 const WebSocket = require('ws');
+const history = require('connect-history-api-fallback');
 
 
 const app = express()
 const port = process.env.PORT || 4000
 
+app.use(history());
 app.use(morgan('combined'));
 app.use(express.static('static/dist'))
 app.use('/api', api);
