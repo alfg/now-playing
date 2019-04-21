49745e7c8ce92687ea6e284f01fb9a7b0ab2fe6e
Alf <alf.g.jr@gmail.com>
2019-04-20 23:41:37 -0700

    line endings to lf

--- static/src/App.vue
+++ static/src/App.vue
@@ -5,14 +5,6 @@
 </template>
 
 <style>
-html, body {
-  /* background: rgb(0, 255, 0) none repeat scroll 0% 0%; */
-  /* background: white; */
-  /* height: 100%; */
-  /* margin: 0; */
-  /* background: transparent; */
-  /* overflow: hidden; */
-}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
